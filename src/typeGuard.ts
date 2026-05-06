5fefbd9b4958670f293f18b650b2845be8e8fe66
Md Akashuzzaman Riaz <akashriaz05@gmail.com>
2026-05-06 23:46:28 +0600

    git merged

--- src/typeGuard.ts
+++ src/typeGuard.ts
@@ -11,3 +11,31 @@ const add = (num1: alphaNumeric, num2: alphaNumeric) => {
     return num1.toString() + num2.toString();
   }
 };
+
+const result1 = add(2, 3);
+
+// console.log(result1);
+
+const result2 = add("2", "3");
+
+// console.log(result2)
+
+// in guard
+type NormalUser = {
+  name: string;
+};
+
+type AdminUser = {
+  name: string;
+  role: "Admin";
+};
+
+const getUserInfo = (user: NormalUser | AdminUser) => {
+  if ("role" in user) {
+    console.log(`This ${user.name} and his role is ${user.role}`);
+  }else{
+    console.log(`this ${user.name } and he is normal user`)
+  }
+};
+
+getUserInfo({ name: "Normal",  });
